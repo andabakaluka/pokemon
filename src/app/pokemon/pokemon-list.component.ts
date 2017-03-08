70420f4f88f800011b40abd30a93f0890592aadc
Luka Andabaka <lukaa.andabaka@gmail.com>
2017-03-08 16:43:44 +0100

    update

---
+++ src/app/pokemon/pokemon-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-list",
+  moduleId: module.id,
+  template: "<h1>Pokémon</h1>"
+})
+export class PokemonListComponent { }
